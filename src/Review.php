9391972107b44da4e4644137260be771b903886b
Adam Binnersley <abinnersley@gmail.com>
2020-12-14 17:02:18 +0000

    Update for new database

--- src/Review.php
+++ src/Review.php
@@ -4,7 +4,18 @@ namespace TheoryTest\Bike;
 
 class Review extends \TheoryTest\Car\Review
 {
-    public $where = ['bikequestion' => 'Y', 'alertcasestudy' => 'IS NULL'];
+    public $where = ['alertcasestudy' => 'IS NULL'];
     
     protected $testType = 'BIKE';
+    
+    /**
+     * Sets the tables
+     */
+    public function setTables()
+    {
+        $this->testsTable = $this->config->table_motorcycle_positions;
+        $this->questionsTable = $this->config->table_motorcycle_questions;
+        $this->learningProgressTable = $this->config->table_motorcycle_progress;
+        $this->progressTable = $this->config->table_motorcycle_test_progress;
+    }
 }
