57f4a1e76c4edd91c18bf049d3e66ebc182e1c4d
Adam Binnersley <AdamB7586@users.noreply.github.com>
2020-12-18 15:09:33 +0000

    Bug fixes

--- src/RandomTest.php
+++ src/RandomTest.php
@@ -14,19 +14,19 @@ class RandomTest extends TheoryTest
     {
         $this->db->delete($this->progressTable, ['user_id' => $this->getUserID(), 'test_id' => $this->testNo, 'type' => $this->getTestType()]);
         $questions = $this->db->query("SELECT * FROM ((SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '1' AND `alertcasestudy` IS NULL LIMIT 2)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '2' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 3)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '3' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 4)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '4' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 3)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '5' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 5)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '6' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 4)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '7' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 2)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '8' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 4)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '9' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 3)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '10' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 4)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '11' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 6)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '12' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 1)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '13' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 3)
-UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '14' AND `bikequestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 1) ORDER BY RAND()) as a
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '2' AND `alertcasestudy` IS NULL LIMIT 3)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '3' AND `alertcasestudy` IS NULL LIMIT 4)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '4' AND `alertcasestudy` IS NULL LIMIT 3)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '5' AND `alertcasestudy` IS NULL LIMIT 5)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '6' AND `alertcasestudy` IS NULL LIMIT 4)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '7' AND `alertcasestudy` IS NULL LIMIT 2)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '8' AND `alertcasestudy` IS NULL LIMIT 4)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '9' AND `alertcasestudy` IS NULL LIMIT 3)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '10' AND `alertcasestudy` IS NULL LIMIT 4)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '11' AND `alertcasestudy` IS NULL LIMIT 6)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '12' AND `alertcasestudy` IS NULL LIMIT 1)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '13' AND `alertcasestudy` IS NULL LIMIT 3)
+UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `dsacat` = '14' AND `alertcasestudy` IS NULL LIMIT 1) ORDER BY RAND()) as a
 UNION (SELECT `prim` FROM `{$this->questionsTable}` WHERE `casestudyno` = '".rand(29, 42)."');");
          
         unset($_SESSION['test'.$this->getTest()]);
